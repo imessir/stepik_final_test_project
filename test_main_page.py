e1b07bf445b36324f85444626a3e71148d7796d3
imessir <iammessor@gmail.com>
2022-03-31 21:10:52 +0400

    Add should_be_login_link method and test

--- test_main_page.py
+++ test_main_page.py
@@ -4,7 +4,14 @@ from selenium.common.exceptions import NoSuchElementException
 LINK = "http://selenium1py.pythonanywhere.com/"
 
 
+def test_guest_should_see_login_link(browser):
+    page = MainPage(browser, LINK)
+    page.open()
+    page.should_be_login_link()
+
+
 def test_guest_can_go_to_login_page(browser):
     page = MainPage(browser, LINK)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
